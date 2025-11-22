156c0e31abb1f1976dcf2f57d0aa31e3b9cebd14
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-22 11:02:15 +0800

    ec2 modification

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,25 @@
 resource "aws_instance" "app" {
-  ami           = "ami-0fa377108253bf620" # Amazon Linux 2023 (Singapore)
+  ami           = "ami-0889a44b331db0194"
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "project-ec2"
+  }
+}
+resource "aws_instance" "app" {
+  ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
 
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2_gp1.id]
-
   associate_public_ip_address = true
 
   tags = {
-    Name = "capstone-gp1-ec2"
+    Name = "capstone-grp1-ec2"
   }
 }
+
