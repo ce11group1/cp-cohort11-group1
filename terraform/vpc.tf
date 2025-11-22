3c6f839cb271932116b21b1625952de7d3a2791c
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-22 11:17:19 +0800

    VPC rename

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags       = { Name = "project-vpc" }
+  tags       = { Name = "capstone-grp1-vpc" }
 }
 
 resource "aws_internet_gateway" "main" {
