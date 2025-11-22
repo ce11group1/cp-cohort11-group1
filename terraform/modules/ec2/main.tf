aa36360a170fb4a1ca2b6e85822f737cc969ae7e
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-22 12:02:00 +0800

    ec2 amend

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "app" {
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
 
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2_gp1.id]
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
   associate_public_ip_address = true
 
   tags = {
--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -1,5 +1,5 @@
 output "security_group_id" {
-  value = aws_security_group.ec2_sg.id
+  value = aws_security_group.ec2_grp1.id
 }
 
 output "instance_id" {
