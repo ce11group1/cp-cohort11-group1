85ce7426e178dcc0c0d8ffe7e5b70db16ceadb64
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-22 10:50:55 +0800

    fmt

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "app" {
   ami           = "ami-0fa377108253bf620" # Amazon Linux 2023 (Singapore)
   instance_type = "t2.micro"
 
-  subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2_gp1.id]
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_gp1.id]
 
   associate_public_ip_address = true
 
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "project-vpc" }
+  tags       = { Name = "project-vpc" }
 }
 
 resource "aws_internet_gateway" "main" {
@@ -8,8 +8,8 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = { Name = "public-subnet" }
