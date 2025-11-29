ecd08e2f28c00330a1cacc800599a31ece3d8fd6
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-29 12:44:06 +0800

    fix fmt

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [aws_security_group.ec2_grp1.id]
--- terraform/modules/iot-core/main.tf
+++ terraform/modules/iot-core/main.tf
@@ -60,14 +60,14 @@ resource "aws_iot_topic_rule" "topic_rule" {
   sql         = "SELECT *, clientid() as device_id FROM '${var.prefix}/${var.env}/data'"
   sql_version = "2016-03-23"
 
-s3 {
-  role_arn    = aws_iam_role.iot_s3_role.arn
-  bucket_name = var.s3_bucket
+  s3 {
+    role_arn    = aws_iam_role.iot_s3_role.arn
+    bucket_name = var.s3_bucket
 
-  key = "raw-data/timestamp=$${timestamp()}/device=$${clientId()}.json"
+    key = "raw-data/timestamp=$${timestamp()}/device=$${clientId()}.json"
 
-  canned_acl = "private"
-}
+    canned_acl = "private"
+  }
 
 
 }
